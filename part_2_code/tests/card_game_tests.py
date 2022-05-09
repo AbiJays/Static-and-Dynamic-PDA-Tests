782d6dcd965b198d38c94a4567d9bde22f25f042
Abi Johnson <99146064+AbiJays@users.noreply.github.com>
2022-05-09 17:18:07 +0100

    Third Method fixed

--- part_2_code/tests/card_game_tests.py
+++ part_2_code/tests/card_game_tests.py
@@ -15,3 +15,9 @@ class TestCardGame(unittest.TestCase):
         self.card1 = Card("Hearts", 4)
         self.card2 = Card("Clubs", 12)
         self.assertEqual(self.card2, self.card_game.highest_card(self.card1, self.card2))
+    
+    def test_cards_total(self):
+        self.card1 = Card("Hearts", 4)
+        self.card2 = Card("Clubs", 12)
+        self.cards = [self.card1, self.card2]
+        self.assertEqual("You have a total of 16", self.card_game.cards_total(self.cards))
