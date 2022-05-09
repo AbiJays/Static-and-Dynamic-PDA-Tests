64f445a5e934a0fa47b0cefa89f63152247738f5
Abi Johnson <99146064+AbiJays@users.noreply.github.com>
2022-05-09 16:40:04 +0100

    Dynamic Testing Method 1

--- part_2_code/tests/card_game_tests.py
+++ part_2_code/tests/card_game_tests.py
@@ -5,7 +5,7 @@ from src.card_game import CardGame
 class TestCardGame(unittest.TestCase):
     
     def test_check_for_ace(self):
-        self.card1 = Card("spades", 1)
+        self.card_game = CardGame()
+        self.card1 = Card("Spades", 1)
         self.assertEqual(True, self.card_game.check_for_ace(self.card1))
 
-    def test_
